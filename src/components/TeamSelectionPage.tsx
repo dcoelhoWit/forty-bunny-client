e87be8ccaab0da8ad402f09a5bab36a5e04fa310
Diogo Coelho <diogo.coelho@wit-software.com>
2025-02-26 15:31:25 +0000

    no message

--- src/components/TeamSelectionPage.tsx
+++ src/components/TeamSelectionPage.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { getTeamColor } from "../utils/TeamColors";
 import WaitingLobby from "./WaitingLobby";
 
