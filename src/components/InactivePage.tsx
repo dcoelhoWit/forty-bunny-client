80eab7442ea8dbbdda4908758a40ef1b754edf05
Diogo Coelho <diogo.coelho@wit-software.com>
2025-02-27 07:56:40 +0000

    no message

--- src/components/InactivePage.tsx
+++ src/components/InactivePage.tsx
@@ -1,8 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+
+// Style
+const Container = styled.div`
+  text-align: center;
+`;
 
 // Component
 export default function InactivePage() {
   return (
-    <div></div>
+    <Container>
+      <h2>
+        Resposta registada.
+      </h2>
+      <h2>
+        Aguardem pela próxima pergunta.
+      </h2>
+    </Container>
   );
 }
--- src/utils/FirebaseSetter.tsx
+++ src/utils/FirebaseSetter.tsx
@@ -2,7 +2,7 @@ import { getDatabase, ref, update } from "firebase/database";
 
 export function setAnswer(questionIndex: string, answer: string, team: string) {
   const db = getDatabase();
-  
+
   update(ref(db, "Votes/" + questionIndex), {
     [team]: answer,
   });
